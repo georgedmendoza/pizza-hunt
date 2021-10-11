28c6144dee6179269bc43e83ddcc5108b989b85c
georgedmendoza <georgedmendoza13@gmail.com>
2021-10-11 18:24:45 -0500

    display all pizzas on frontend

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -5,28 +5,45 @@ const pizzaController = {
     // get all pizzas
     getAllPizza(req,res) {
         Pizza.find({})
-        .then(dbPizzaData => res.json(dbPizzaData))
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        });
+            // to get the actual comment and not just the id
+            .populate({
+                path: 'comments',
+                // minus sign indicates to not be returned
+                select: '-__v'
+            })
+            // doesn't include v field of pizza
+            .select('-__v')
+            // return by newest pizza
+            .sort({ _id: -1 })
+            .then(dbPizzaData => res.json(dbPizzaData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
     },
 
     // get one pizza by id
     getPizzaById({ params }, res) {
         Pizza.findOne({ _id: params.id })
-        .then(dbPizzaData => {
-            // If no pizza is found, send 404
-            if (!dbPizzaData) {
-                res.status(404).json({ message: 'No pizza found with this id!' });
-                return;
-            }
-            res.json(dbPizzaData);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        });
+            .populate({
+                path: 'comments',
+                // minus sign indicates to not be returned
+                select: '-__v'
+            })
+            // doesn't include v field of pizza
+            .select('-__v')
+            .then(dbPizzaData => {
+                // If no pizza is found, send 404
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
     },
 
     // createPizza
